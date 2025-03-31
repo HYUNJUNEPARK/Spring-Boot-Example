3189d3face51821cf4d242806a22e681ef392910
HYUNJUNE <june2ac4dev@gmail.com>
2025-03-31 09:25:20 +0900

    2-15

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RequestMapping("/answer")
 @RequiredArgsConstructor
 @Controller
@@ -20,6 +23,9 @@ public class AnswerController {
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+
+        log.info("createAnswer() id={}, content={}", id, content);
+
         Question question = this.questionService.getQuestion(id);
         this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);
