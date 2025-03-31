231fa7db1f24c93e7eaa0e5f804605d611c77cdd
HYUNJUNE <june2ac4dev@gmail.com>
2025-03-31 14:37:19 +0900

    2-16

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,13 +2,14 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,13 +23,22 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+    public String createAnswer(
+        Model model,
+        @PathVariable("id") Integer id,
+        @Valid AnswerForm answerForm,
+        BindingResult bindingResult
+        //@RequestParam(value="content") String content
+    ) {
 
-        log.info("createAnswer() id={}, content={}", id, content);
-
+        //log.info("createAnswer() id={}, content={}", id, content);
+        //에러 발생 시 question_detail 페이지 재오픈
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
-
 }
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,14 +1,14 @@
 package com.mysite.sbb.question;
 
-import java.util.List;
-
+import com.mysite.sbb.answer.AnswerForm;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 /*
  컨트롤러 -> 서비스 -> 리포지터리 -> 데이터를 템플릿에 전달
@@ -38,9 +38,37 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(
+        Model model,
+        @PathVariable("id") Integer id,
+        AnswerForm answerForm
+    ) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    /**
+     * @param questionForm
+     * @param bindingResult: subject, content 항목을 지닌 폼이 전송되면 QuestionForm의 subject, content 속성이 자동으로 바인딩
+     *
+     * BindingResult 매개변수는 항상 @Valid 매개변수 바로 뒤에 위치해야 한다. 만약 두 매개변수의 위치가 정확하지 않다면 @Valid만 적용되어 입력값 검증 실패 시 400 오류가 발생한다.
+     */
+    @PostMapping("/create")
+    public String questionCreate(
+        @Valid QuestionForm questionForm,
+        BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "question_form";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
     }
 }
