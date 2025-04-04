b408dc5745d074d07ad52ba1cc293646a0b7d318
HYUNJUNE <june2ac4dev@gmail.com>
2025-04-04 17:21:22 +0900

    3-09(1)

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,11 +6,13 @@ import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -93,5 +95,47 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate(QuestionForm questionForm) {
         return "question_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(
+        QuestionForm questionForm,
+        @PathVariable("id") Integer id,
+        Principal principal
+    ) {
+        //현재 로그인한 사용자와 질문의 작성자가 동일하지 않을 경우
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        //수정할 질문의 제목과 내용을 화면에 보여주기 위해 questionForm 객체에 id 값으로 조회한 subject, object 내용을 담아서 템플릿에 전달한다.
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+
+        return "question_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(
+        @Valid QuestionForm questionForm,
+        BindingResult bindingResult,
+        Principal principal,
+        @PathVariable("id") Integer id
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
